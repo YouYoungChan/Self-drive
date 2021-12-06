866fc9efd3544182570a319401e7972650d3104d
YouYoungChan <yyc30734@naver.com>
2021-12-06 23:15:06 +0900

    Finalmodify

--- Robo/script/FinalTest.py
+++ Robo/script/FinalTest.py
@@ -8,20 +8,26 @@ import numpy as np
 
 def avg_range(ranges):
     range_arr = np.array(ranges)
-    p_40 = range_arr[0:40]
-    m_40 = range_arr[-40: ]
-    p_80 = range_arr[40:80]
-    m_80 = range_arr[-80:-40]
-    p_40_real = p_arr[p_arr > 0]
-    m_40_real = m_arr[m_arr > 0]
-    p_80_real = p_80[p_80 > 0]
-    m_80_real = m_80[m_80 > 0]
-    p_40_mean = np.mean(p_arr_real)
-    m_80_mean = np.mean(m_arr_real)
-    p_80_mean = np.mean(p_80_real)
-    m_80_mean = np.mean(m_80_real)
-    print("p40 : ", p_40_mean, "\n M40 : ", m_40_mean , "\n p80 : ", p_80_mean, "\n M80 : ", m_80_mean )
-    return p_40_mean,m_40_mean,p_80_mean,m_80_mean
+    p_10 = range_arr[0:10]
+    m_10 = range_arr[-10: ]
+    p_30 = range_arr[10:30]
+    m_30 = range_arr[-30:-10]
+    p_70 = range_arr[30:70]
+    m_70 = range_arr[-70:-30]
+    p_10_real = p_10[p_10 > 0]
+    m_10_real = m_10[m_10 > 0]
+    p_30_real = p_30[p_30 > 0]
+    m_30_real = m_30[m_30 > 0]
+    p_70_real = p_70[p_70 > 0]
+    m_70_real = m_70[m_70 > 0]
+    p_10_mean = np.mean(p_10_real)
+    m_10_mean = np.mean(m_10_real)
+    p_30_mean = np.mean(p_30_real)
+    m_30_mean = np.mean(m_30_real)
+    p_70_mean = np.mean(p_70_real)
+    m_70_mean = np.mean(m_70_real)
+#    print("P10 : ",p_10_mean, "\nM10 : ",m_10_real,"\np30 : ", p_30_mean, "\nM30 : ", m_30_mean , "\np70 : ", p_70_mean, "\nM70 : ", m_70_mean )
+    return p_10_mean,m_10_mean,p_30_mean,m_30_mean,p_70_mean,m_70_mean
 
 class SelfDrive:
     def __init__(self, publisher):
@@ -31,20 +37,24 @@ class SelfDrive:
     def lds_callback(self, scan):
         # scan 분석 후 속도 결정
         # ...
-        print("scan[0]:", scan.ranges[0])
         turtle_vel = Twist()
          # 전진 속도 및 회전 속도 지정
-        plus_avg, minus_avg, p_80, m_80 = avg_range(scan.ranges)
+        p_10_avg, m_10_avg, p30_avg, m30_avg, p70_avg, m70_avg = avg_range(scan.ranges)
 
-        if p_80 < 0.30 or m_80 < 0.30:
-            if p_80 >= m_80:
+        if p_10_avg < 0.25 or m_10_avg < 0.25:
+            turtle_vel.linear.x = 0.03
+            turtle_vel.angular.z = 2        
+
+        elif p30_avg < 0.30 or m30_avg < 0.30:
+            turtle_vel.linear.x = 0
+            if p30_avg >= m30_avg:
                 turtle_vel.angular.z = 2.0
             else:
                 turtle_vel.angular.z = -2.0
 
-        if plus_avg < 0.30 or minus_avg < 0.30:
-            turtle_vel.linear.x = 0
-            if plus_avg >= minus_avg:
+        elif p70_avg < 0.16 or m70_avg < 0.16:
+            turtle_vel.linear.x = 0.05
+            if p70_avg >= m70_avg:
                 turtle_vel.angular.z = 2.0
             else:
                 turtle_vel.angular.z = -2.0
